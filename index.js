fe23c37a1dba1b996063141300b1ec97b5668308
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2021-10-24 21:07:07 +0500

    Adding files

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.DATABASE_CONNECTION_URI;
 
-const PORT = process.env.PORT || 5000;
+console.log(CONNECTION_URL)
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>app.listen(PORT,()=>{
