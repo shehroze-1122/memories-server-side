2890a90e545ea9afb8de3a0b482fbd9ba7f4370d
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2021-10-25 09:16:10 +0500

    debug

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,13 +3,13 @@ import postModel from "../models/postModel.js";
 
 export const getPosts= async (req, res)=>{
 
-    res.status(200).json('Working posts route')
-//     try {
-//        const posts = await postModel.find({});
-//        res.status(200).json(posts);
-//    } catch (error) {
-//        res.status(404).json({message:error.message});
-//    }
+    // res.status(200).json('Working posts route')
+    try {
+       const posts = await postModel.find({});
+       res.status(200).json(posts);
+   } catch (error) {
+       res.status(404).json({message:error.message});
+   }
 }
 
 export const addPost = async (req, res)=>{
