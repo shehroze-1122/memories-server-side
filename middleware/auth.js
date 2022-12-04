3fd0304b54da804e2341d57db3a31681f5dfb85d
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2022-12-04 14:42:12 +0500

    improve error handling in the auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,31 @@
 import jwt from 'jsonwebtoken'
 
 const auth = (req, res, next) => {
+  const authorization = req.headers.authorization
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Authorization header must be provided' })
+  }
+
+  const token = authorization.split(' ')[1]
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization header must contain Bearer followed by the token' })
+  }
+  const isCustomToken = token.length < 500
+
+  let decodedData
   try {
-    const token = req.headers.authorization.split(' ')[1]
-
-    const isCustomToken = token.length < 500
-
-    let decodedData
-
     if (isCustomToken && token) {
       decodedData = jwt.verify(token, 'test')
       req.userId = decodedData?.id
     } else {
       decodedData = jwt.decode(token)
-
       req.userId = decodedData?.sub
     }
-
-    next()
   } catch (error) {
-    res.status(401).send({ message: 'Invalid or expired token' })
+    return res.status(401).json({ message: 'Invalid or expired token' })
   }
+  next()
 }
 
 export default auth
